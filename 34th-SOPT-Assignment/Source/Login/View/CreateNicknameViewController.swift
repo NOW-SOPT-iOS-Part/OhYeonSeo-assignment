cbffbcf059ba12916f33075a523eb2367efaff2d
luckyyy <oyslucy@naver.com>
2024-06-07 09:47:34 +0900

    [refact] LoginView 분리

---
+++ 34th-SOPT-Assignment/Source/Login/View/CreateNicknameViewController.swift
@@ -0,0 +1,8 @@
+//
+//  CreateNicknameViewController.swift
+//  34th-SOPT-Assignment
+//
+//  Created by 오연서 on 6/7/24.
+//
+
+import Foundation
---
+++ 34th-SOPT-Assignment/Source/Login/View/LoginView.swift
@@ -0,0 +1,8 @@
+//
+//  LoginView.swift
+//  34th-SOPT-Assignment
+//
+//  Created by 오연서 on 6/7/24.
+//
+
+import Foundation
